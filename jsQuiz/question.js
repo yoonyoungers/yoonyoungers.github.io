348e63423fd0cc3d9e03e137e75a7df0ed411ffa
Yoon Young <yoonyouk@andrew.cmu.edu>
2019-09-30 11:03:15 -0400

    webpageeee

---
+++ jsQuiz/question.js
@@ -0,0 +1,45 @@
+class Question {
+   constructor (text, choices, answer){
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+
+
+    isCorrectAnswer(choice){
+             return this.answer === choice;
+            }
+
+}
+}
+
+var questions = [
+new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
+new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+new Question("What has not been covered in PUI lab?", ["HTML", "CSS","Javascript", "Java"], "Java"),
+];
+
+class Quiz {
+        constructor(questions) {
+            this.score = 0;
+    this.questions = questions;
+            this.questionIndex = 0;
+
+
+    guess(answer) {
+    if(this.getQuestion().isCorrectAnswer(answer)) {
+     this.score++;
+            }
+        
+        this.questionIndex++;
+}
+
+}   
+
+    
+    getQuestion() {
+
+        return this.questions[this.questionIndex];
+    
+}
+
+}
